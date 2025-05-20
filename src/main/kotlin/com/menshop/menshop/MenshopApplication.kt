6480ed4b3f019303abcdf7df79a180aabc983e29
raimauricio <rai_mauricio@hotmail.com>
2025-05-20 17:12:37 -0300

    feat: estrutura inicial api MensShop

---
+++ src/main/kotlin/com/menshop/menshop/MenshopApplication.kt
@@ -0,0 +1,11 @@
+package com.menshop.menshop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MenshopApplication
+
+fun main(args: Array<String>) {
+	runApplication<MenshopApplication>(*args)
+}
---
+++ src/test/kotlin/com/menshop/menshop/MenshopApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.menshop.menshop
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MenshopApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
