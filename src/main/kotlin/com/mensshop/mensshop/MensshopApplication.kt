052e3625852640410549fcbe6515a38ded1a26cc
raimauricio <rai_mauricio@hotmail.com>
2025-05-25 08:06:59 -0300

    Commit Inicial projeto WebApi Mens Shop

---
+++ src/main/kotlin/com/mensshop/mensshop/MensshopApplication.kt
@@ -0,0 +1,11 @@
+package com.mensshop.mensshop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MensshopApplication
+
+fun main(args: Array<String>) {
+	runApplication<MensshopApplication>(*args)
+}
---
+++ src/test/kotlin/com/mensshop/mensshop/MensshopApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mensshop.mensshop
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MensshopApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
